a30fd9cbc8a93c2785983b36921ff928273bdfff
Bilal Mahmood <bmood1@gmail.com>
2021-05-09 03:22:55 -0500

    Initial commit

---
+++ src/main/java/com/mahmood/OsrsDb/OsrsDbApplication.java
@@ -0,0 +1,13 @@
+package com.mahmood.OsrsDb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsrsDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsrsDbApplication.class, args);
+	}
+
+}
